740780a982a80ce6d17525463e3cc62165c6d856
Andrés Milla <andressmilla@gmail.com>
2021-05-14 17:35:26 -0300

    Refactor: Renamed variables and added optional args to OwlMovieRepository

--- TP4-Linked-data/src/db/repository.py
+++ TP4-Linked-data/src/db/repository.py
@@ -4,15 +4,28 @@ from constant import NAMESPACES
 
 class OwlMovieRepository():
     @classmethod
-    def read(cls, graph_location, namespaces=NAMESPACES):
+    def read(cls, graph_location, namespaces=None, file_format="turtle"):
         graph = Graph()
 
-        graph.parse(str(graph_location), format="turtle")
+        if namespaces is not None:
+            cls.__bind_namespaces(graph, namespaces)
+
+        graph.parse(str(graph_location), format=file_format)
 
         return graph
 
     @classmethod
-    def write(self, path_file, graph):
+    def write(cls, path_file, graph, namespaces=None, file_format="turtle"):
+        if namespaces is not None:
+            cls.__bind_namespaces(graph, namespaces)
+
         with open(path_file, "w") as file:
-            serialized_graph = graph.serialize(format="turtle").decode("utf-8")
+            serialized_graph = graph.serialize(
+                format=file_format).decode("utf-8")
+
             file.write(serialized_graph)
+
+    @classmethod
+    def __bind_namespaces(cls, graph, namespaces):
+        for prefix, uri in NAMESPACES.items():
+            graph.bind(prefix, uri)
